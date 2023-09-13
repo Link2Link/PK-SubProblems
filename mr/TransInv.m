04a876034930658fc3a4a022a4fa210671a38447
WANG CHENFEI <barrycf.wang@gmail.com>
2023-09-13 20:48:46 +0800

    update

--- mr/TransInv.m
+++ mr/TransInv.m
@@ -5,7 +5,6 @@ function invT = TransInv(T)
 % Uses the structure of transformation matrices to avoid taking a matrix
 % inverse, for efficiency.
 % Example Input:
-% 
 % clear; clc;
 % T = [[1, 0, 0, 0]; [0, 0, -1, 0]; [0, 1, 0, 3]; [0, 0, 0, 1]];
 % invT = TransInv(T)
